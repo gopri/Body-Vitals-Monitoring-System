424a936899e42965d5c21413bcc9caab99b77cb9
Priyanka Goyal <goyalpriyanka.pg@gmail.com>
2018-11-22 19:50:06 -0800

    Add files via upload

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,25 +1,26 @@
 $(document).ready(function () {
                   var timeData = [],
-                  intensityData = [],
+                  heartRateData = [],
                   threshold = 0,
                   defaultThreshold = 250,
                   initialValues = [],
-                  setData = [],
+                  tempData = [],
                   repCount = 0,
-                  repData = [];
+                  spo2Data = [];
+
                   var data = {
                   labels: timeData,
                   datasets: [
                              {
                              fill: false,
-                             label: 'Intensity',
+                             label: 'Heart Rate',
                              yAxisID: 'Peaks',
                              borderColor: "rgba(255, 204, 0, 1)",
                              pointBoarderColor: "rgba(255, 204, 0, 1)",
                              backgroundColor: "rgba(255, 204, 0, 0.4)",
                              pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
                              pointHoverBorderColor: "rgba(255, 204, 0, 1)",
-                             data: intensityData
+                             data: heartRateData
                              }
                              ]
                   }
@@ -29,14 +30,14 @@ $(document).ready(function () {
                   datasets: [
                              {
                              fill: false,
-                             label: 'Rep',
-                             yAxisID: 'Rep',
+                             label: 'SPO2',
+                             yAxisID: 'SPO2',
                              borderColor: "rgba(255, 204, 0, 1)",
                              pointBoarderColor: "rgba(255, 204, 0, 1)",
                              backgroundColor: "rgba(255, 204, 0, 0.4)",
                              pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
                              pointHoverBorderColor: "rgba(255, 204, 0, 1)",
-                             data: repData
+                             data: spo2Data
                              }
                              ]
                   }
@@ -46,14 +47,14 @@ $(document).ready(function () {
                   datasets: [
                              {
                              fill: false,
-                             label: 'Set',
-                             yAxisID: 'Set',
+                             label: 'Temperature',
+                             yAxisID: 'Temperature',
                              borderColor: "rgba(255, 204, 0, 1)",
                              pointBoarderColor: "rgba(255, 204, 0, 1)",
                              backgroundColor: "rgba(255, 204, 0, 0.4)",
                              pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
                              pointHoverBorderColor: "rgba(255, 204, 0, 1)",
-                             data: setData
+                             data: tempData
                              }
                              ]
                   }
@@ -61,15 +62,15 @@ $(document).ready(function () {
                   var basicOption3 = {
                   title: {
                   display: true,
-                  text: 'Set Real-time Data',
-                  fontSize: 36
+                  text: 'Temperature Real-time Data',
+                  fontSize: 32
                   },
                   scales: {
                   yAxes: [{
-                          id: 'Set',
+                          id: 'Temperature',
                           type: 'linear',
                           scaleLabel: {
-                          labelString: 'Set',
+                          labelString: 'Temperature',
                           display: true
                           },
                           position: 'left',
@@ -80,15 +81,15 @@ $(document).ready(function () {
                   var basicOption2 = {
                   title: {
                   display: true,
-                  text: 'Rep Real-time Data',
-                  fontSize: 36
+                  text: 'SPO2 Real-time Data',
+                  fontSize: 32
                   },
                   scales: {
                   yAxes: [{
-                          id: 'Rep',
+                          id: 'SPO2',
                           type: 'linear',
                           scaleLabel: {
-                          labelString: 'Rep',
+                          labelString: 'SPO2',
                           display: true
                           },
                           position: 'left',
@@ -99,15 +100,15 @@ $(document).ready(function () {
                   var basicOption = {
                   title: {
                   display: true,
-                  text: 'Intensity Real-time Data',
-                  fontSize: 36
+                  text: 'Heart Rate Real-time Data',
+                  fontSize: 32
                   },
                   scales: {
                   yAxes: [{
                           id: 'Peaks',
                           type: 'linear',
                           scaleLabel: {
-                          labelString: 'Intensity',
+                          labelString: 'Heart Rate',
                           display: true
                           },
                           position: 'left',
@@ -127,65 +128,67 @@ $(document).ready(function () {
                                               });
                   
                   var myLineChart2 = new Chart(ctx2, {
-                                              type: 'bar',
+                                              type: 'line',
                                               data: data2,
                                               options: basicOption2
                                               });
                   
                   var myLineChart3 = new Chart(ctx3, {
-                                               type: 'bar',
+                                               type: 'line',
                                                data: data3,
                                                options: basicOption3
                                                });
                   
                   var ws = new WebSocket('wss://' + location.host);
-                  ws.onopen = function () {
-                  console.log('Successfully connect WebSocket');
-                  }
-                  ws.onmessage = function (message) {
-                  console.log('receive message' + message.data);
-                  try {
-                  var obj = JSON.parse(message.data);
-                  var step = 0;
-                 
-                  if(obj.voltage>450){
-                      step = 1;
-                  }
-                  
-                  var flexCount = 0;
-                  for(var i=0;i<repData.length;i++){
-                      if(repData[i]==1){
-                          flexCount++;
-                      }
-                  }
-                  var avgSetsDone = flexCount/12;
-                  
-                  timeData.push(obj.time);
 
-                  intensityData.push(obj.red);
-                  /*if(obj.voltage>450){
-                      intensityData.push(obj.voltage);
-                  }else{
-                      intensityData.push(450);
-                  }*/
-                  
-                  repData.push(step);
-                  setData.push(avgSetsDone);
-                  // only keep no more than 50 points in the line chart
-                  const maxLen = 50;
-                  var len = timeData.length;
-                  if (len > maxLen) {
-                  timeData.shift();
-                  intensityData.shift();
-                  repData.shift();
-                  setData.shift();
+                  ws.onopen = function () {
+                    console.log('Successfully connect WebSocket');
                   }
-                  
-                  myLineChart.update();
-                  myLineChart2.update();
-                  myLineChart3.update();
-                  } catch (err) {
-                  console.error(err);
-                  }
+
+                  ws.onmessage = function (message) {
+                    console.log('receive message' + message.data);
+                    try {
+                        var obj = JSON.parse(message.data);
+                        var step = 0;
+                        
+                        if(obj.voltage>450){
+                            step = 1;
+                        }
+                        
+                        var flexCount = 0;
+                        for(var i=0;i<spo2Data.length;i++){
+                            if(spo2Data[i]==1){
+                                flexCount++;
+                            }
+                        }
+                        var avgSetsDone = flexCount/12;
+                        
+                        timeData.push(obj.time);
+
+                        heartRateData.push(obj.red);
+                        /*if(obj.voltage>450){
+                            intensityData.push(obj.voltage);
+                        }else{
+                            intensityData.push(450);
+                        }*/
+                        
+                        spo2Data.push(obj.IR);
+                        tempData.push(obj.green);
+                        // only keep no more than 50 points in the line chart
+                        const maxLen = 50;
+                        var len = timeData.length;
+                        if (len > maxLen) {
+                            timeData.shift();
+                            heartRateData.shift();
+                            spo2Data.shift();
+                            tempData.shift();
+                        }
+                        
+                        myLineChart.update();
+                        myLineChart2.update();
+                        myLineChart3.update();
+                    } catch (err) {
+                        console.error(err);
+                    }
                   }
 });
