f3f3ccbbb67da59090d57c7919e8147b10e523ff
Priyanka Goyal <goyalpriyanka.pg@gmail.com>
2018-12-14 02:33:58 -0800

    Add files via upload

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -228,7 +228,7 @@ $(document).ready(function () {
                             minTemp = obj.red
                         }
 
-                        ageSet = document.getElementById("demo").value;
+                        ageSet = document.getElementById("demo").innerHTML;
                         console.log(ageSet);
                         console.log(220-parseInt(ageSet));
                         console.log($("#demo"));
