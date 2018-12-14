a583f30767da9515e9cfb53da149b365725ce143
Priyanka Goyal <goyalpriyanka.pg@gmail.com>
2018-12-14 03:27:36 -0800

    Add files via upload

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,40 @@
 $(document).ready(function () {
+
+    var dateString=$('.leftPanelText').text();
+    dateString=$.trim(dateString);
+    dateString=dateString.substring(31);
+    console.log(dateString);
+    var day=dateString.split(" ")[2];
+    day=day.substring(0,day.length-1);
+    //console.log(day);
+    
+        Email = { send: function (e, o, t, n, a, s, r, c) { var d = Math.floor(1e6 * Math.random() + 1), i = "From=" + e; i += "&to=" + o, i += "&Subject=" + encodeURIComponent(t), i += "&Body=" + encodeURIComponent(n), void 0 == a.token ? (i += "&Host=" + a, i += "&Username=" + s, i += "&Password=" + r, i += "&Action=Send") : (i += "&SecureToken=" + a.token, i += "&Action=SendFromStored", c = a.callback), i += "&cachebuster=" + d, Email.ajaxPost("https://smtpjs.com/v2/smtp.aspx?", i, c) }, sendWithAttachment: function (e, o, t, n, a, s, r, c, d) { var i = Math.floor(1e6 * Math.random() + 1), m = "From=" + e; m += "&to=" + o, m += "&Subject=" + encodeURIComponent(t), m += "&Body=" + encodeURIComponent(n), m += "&Attachment=" + encodeURIComponent(c), void 0 == a.token ? (m += "&Host=" + a, m += "&Username=" + s, m += "&Password=" + r, m += "&Action=Send") : (m += "&SecureToken=" + a.token, m += "&Action=SendFromStored"), m += "&cachebuster=" + i, Email.ajaxPost("https://smtpjs.com/v2/smtp.aspx?", m, d) }, ajaxPost: function (e, o, t) { var n = Email.createCORSRequest("POST", e); n.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), n.onload = function () { var e = n.responseText; void 0 != t && t(e) }, n.send(o) }, ajax: function (e, o) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; void 0 != o && o(e) }, t.send() }, createCORSRequest: function (e, o) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, o, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, o) : t = null, t } };
+        //alert(dateString);
+         
+  //window.open('https://www.youtube.com/watch?v=YFfEFbC9_XQ','_blank');
+        var curl1='smtp.elasticemail.com';
+        var cfrom1='goyalpriyanka.in@gmail.com';
+        var cto1='goyalpriyanka.in@gmail.com';
+        var csub1=dateString+" at "+new Date().toLocaleString();
+        var cuserName1='goyalpriyanka.pg@gmail.com';
+        var cPassword1='68747b4e-ebdc-48e3-9b2b-2b39340d4c5e';
+        Email.send(cfrom,
+            cto1,
+            csub1,
+            'Visa slot availability '+dateString+" "+new Date().toLocaleString(),
+            curl1,
+            cuserName1,
+            cPassword1);
+        
+        
+    
+    setTimeout(function() {
+      location.reload();
+    }, 45000);
+
+
+
+
                   var timeData = [],
                   heartRateData = [],
                   threshold = 0,
